38ed2db026aa6facfcb2755403bf321972f0b2f4
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2019-03-29 10:01:10 +0000

    convert to sourcing data from sql scripts

--- arctic/arctic-data-processor.r
+++ arctic/arctic-data-processor.r
@@ -1,27 +1,68 @@
-#-- script to process the arctic clinical data and create the cohort
-#-- uses the consent manifest as the determinant of which participants
-#-- make it into the final cohort
-#-- the consent manifest lives at https://my.huddle.net/workspace/38658629/files/#/59413080
-
+#-- script to refresh the arctic dataset given new data tables
 #-- setup
 rm(list = objects())
 options(stringsAsFactors = F,
 	scipen = 200)
 library(wrangleR)
+library(tidyverse)
+library(lubridate)
+library(RPostgreSQL)
+p <- getprofile("indx_con")
+drv <- dbDriver("PostgreSQL")
+indx.con <- dbConnect(drv,
+             dbname = "cohorts",
+             host = p$host,
+             port = p$port,
+             user = p$user,
+             password = p$password)
 
-#-- for reading excel files
-library(gdata)
+#-- get trialno table, consent manifest, and sequencing manifest
+trialno <- dbGetQuery(indx.con, "select * from arctic_v2.trialno;")
+consent_manifest <- dbGetQuery(indx.con, "select * from cll_common.consent_manifest where trial in ('Arctic');")
 
-#-- FUNCTIONS
-#-- function to read in the file as a dataframe
-readfile <- function(filename){
-	read.table(paste0("../received-clinical-datasets/arctic/", filename),
-		      na.strings = c("", "NA"),
-		      comment.char = "",
-		      sep = "|",
-		      header = T
-		      )
+#-- get list of sql files which make research tables and read them into memory
+sql_scripts <- list.files(path = "./sql-scripts", pattern = "*.sql", full.names = T)
+d <- lapply(sql_scripts, function(x) dbGetQuery(indx.con, paste(readLines(x), collapse = " ")))
+names(d) <- gsub(".sql", "", basename(sql_scripts))
+
+#-- disconnect from dbs
+dbdisconnectall()
+
+#-- select participants to take forward
+#-- have valid consent or are died and don't have any outstanding queries
+consent_manifest$export_to_research <- (consent_manifest$valid_consent | 
+					consent_manifest$patient_deceased) &
+					!consent_manifest$outstanding_consent_query
+cohort_trialno <- consent_manifest$trialno[consent_manifest$export_to_research &
+					   !is.na(consent_manifest$trialno)]
+cohort_patno <- trialno$patno[trialno$trialno %in% cohort_trialno]
+
+#-- select genomes to include, any genome associated with a valid trialno
+cohort_patientid <- consent_manifest$patientid[consent_manifest$export_to_research &
+					       !is.na(consent_manifest$patientid)]
+
+#-- do some checks before proceeding
+if(any(duplicated(cohort_trialno))){
+	stop("duplicated trialnos in cohort (duplicated entries in consent manifest)")
 }
+if(any(duplicated(cohort_patientid))){
+	stop("duplicated patientids in cohort (duplicated entries in consent manifest)")
+}
+if(any(duplicated(cohort_patno))){
+	stop("duplicated patnos in cohort (duplicated entries in trialno table)")
+}
+if(any(!sapply(d, function(x) "patno" %in% colnames(x)))){
+	stop("patno not in every table")
+}
+if(any(sapply(d[!names(d) %in% "genomes"], function(x) any(is.na(x$patno))))){
+	stop("missing values in patno")
+}
+
+#-- go through and remove non-valid patnos from non-genomes tables
+d_exp <- lapply(d[!names(d) %in% "genomes"], function(x) x[x$patno %in% cohort_patno,])
+
+#-- remove rows from genomes by patientid (dont' have to clinical data to be in this table)
+d_exp[["genomes"]] <- d[["genomes"]] %>% filter(patientid %in% cohort_patientid & !is.na(patientid))
 
 #-- function to write out files
 writefile <- function(df, filename, separator = "\t"){
@@ -31,159 +72,12 @@ writefile <- function(df, filename, separator = "\t"){
 		    row.names = F,
 		    quote = F,
 		    na= "")
+	cat(paste("--", filename, "written\n"))
 }
 
-#-- function to get how many columns (separators) in file?
-#-- essentially get awk to count number of separators per line (deleting nulls first), then do sort and uniq
-colcount <- function(file, separator = "\t"){
-	numcols <- as.numeric(strsplit(system(paste0("cat ", file, " | tr -d '\\000' | awk -F'", separator, "' '{print NF}' | sort | uniq"), intern = T), " "))
-	stopifnot(length(numcols) == 1)
-	return(numcols)
+#-- write out tables for research
+dir.create("research-data")
+dir.create(file.path("research-data", Sys.Date()))
+for(i in names(d_exp)){
+	writefile(d_exp[[i]], file.path("research-data", Sys.Date(), i))
 }
-
-#-- function to get linecount of a file
-linecount <- function(file){
-	as.numeric(strsplit(trimws(system(paste("wc -l", file), intern = T)), " ")[[1]][1])
-}
-
-#-- PROCESS CLINICAL DATA FILES
-#-- get list of txt files in the cll210 directory
-files <- list.files(path = "../received-clinical-datasets/arctic", pattern = "txt$") 
-
-#-- for each of the files, read them in
-dfs <- lapply(files, function(x) readfile(x))
-
-#-- make tidier names
-names(dfs) <- gsub(".txt", "", basename(files))
-
-#-- make number of rows and cols for each
-dims.ls <- lapply(dfs, function(x) c(nrow(x), ncol(x)))
-dims.df <- as.data.frame(do.call(rbind, dims.ls))
-names(dims.df) <- c("nrows", "ncols")
-
-#-- write out the dimensions
-write.table(dims.df, file = "arctic_tabledims.txt", sep = "\t", row.names = F)
-
-#-- get the column headings per table, and number of NAs
-field.names <- lapply(dfs, function(x) names(x))
-n.missing <- lapply(dfs, function(x) apply(x, 2, function(y) sum(is.na(y))))
-n.rows <- lapply(dfs, function(x) apply(x, 2, function(y) length(y)))
-
-#-- convert to data.frame then writeout
-n.missing.df <- as.data.frame(unlist(n.missing))
-n.rows.df <- as.data.frame(unlist(n.rows))
-cll210.summ <- setNames(cbind(n.missing.df, n.rows.df), c("n.missing", "n.rows"))
-write.table(cll210.summ, file = "arctic_data_summary.txt", sep = "\t")
-
-#-- COHORT SELECTION
-#-- read in Excel consent manifest file, then drop the unnecessaries
-consent.manifest <- read.xls("../CLL consent spreadsheet_MASTER.xlsx", stringsAsFactors = F)
-consent.manifest <- dropnrename(consent.manifest,
-				c("Row.ID"
-				  , "Trial.Name"
-	,"BiobankPatientID"
-	,"Trial.Number."
-	,"Valid.Consent"
-	,"Outstanding.consent.query"
-	,"Patient.deceased"
-	,"Partial.consent"),
-				c("consent.id"
-				  , "Trial"
-	,"PatientID"
-	,"TrialNo"
-	,"Valid.consent"
-	,"Outstanding.consent.query"
-	,"Patient.deceased"
-	,"Partial.consent")
-	)
-
-#-- read in the genome manifest file
-#-- assembled from https://my.huddle.net/workspace/29344763/files/#/folder/43829733/list
-#-- processed by cllrialto-genome-sample-manifest-processor.r
-genome.manifest <- readRDS("../cll-genomes-manifest.rds")
-
-#-- only interested in the CLL210 participants at this point
-consent.manifest <- consent.manifest[consent.manifest$Trial == "Arctic",]
-
-#-- read in the file that links TrialNo to PatNo, provided by Fabrizio
-participant.manifest <- read.table("../received-clinical-datasets/arctic/trialnos.csv", sep = "|", header = T)
-
-#-- need to convert date of birth to year of birth
-#-- function to make year from a date
-to.year.fun <- function(x){
-	as.numeric(format(as.Date(x), "%Y"))
-}
-
-#-- apply it to the fields
-dfs[["rand"]]$YOBF03 <- to.year.fun(dfs[["rand"]]$DOBF03)
-
-#-- remove the original fields
-dfs[["rand"]] <- dfs[["rand"]][,!colnames(dfs[["rand"]]) %in% "DOBF03"]
-
-#-- merge in the details from consent manifest
-participant.manifest <- merge(participant.manifest,
-			      consent.manifest,
-			      by = "TrialNo",
-			      all = T)
-
-#-- flag those who have a genome
-participant.manifest$in.genome.manifest <- participant.manifest$PatientID %in% genome.manifest$PatientID
-
-#-- select participants to take forward
-#-- have a genome AND (got valid consent OR are deceased) AND don't have an outstanding consent query AND have a PersonID
-participant.manifest$export.to.research  <- ( participant.manifest$Valid.consent | participant.manifest$Patient.deceased ) & !participant.manifest$Outstanding.consent.query
-
-#-- write out the participant manifest
-write.table(participant.manifest, "arctic-participant-manifest.txt", row.names = F, sep = "\t")
-
-ids.to.include <- participant.manifest$PatNo[participant.manifest$export.to.research]
-
-#-- columns to EXCLUDE
-cols.to.remove <- readLines("arctic-drop-fields.txt")
-
-#-- check that all columns actually feature in the datasets
-stopifnot(all(cols.to.remove %in% unlist(field.names)))
-
-#-- merge in trialNo to each table, using PatNo as the thing to merge on
-#-- not all tables have PatNo though so only apply to those with that data, need to put names back in afterwards
-gotpatno <- sapply(dfs, function(x) "PatNo" %in% colnames(x))
-n <- names(dfs)
-dfs[gotpatno] <- lapply(dfs[gotpatno], function(x)
-	      merge(x, participant.manifest[,c("TrialNo", "PatNo")], by = "PatNo", all.x = T))
-
-#-- make new list of dataframes that only have those participants we want to include, and don't have cols to remove
-dfs.export <- dfs
-dfs.export[gotpatno] <- lapply(dfs.export[gotpatno], function(x) x[x$PatNo %in% ids.to.include , !names(x) %in% cols.to.remove])
-names(dfs) <- n
-
-#-- add in genomes table using genome.manifest
-#-- filter for valid PatientID
-dfs.export[["genomes"]] <- genome.manifest[genome.manifest$PatientID %in% participant.manifest$PatientID[which(participant.manifest$export.to.research)],]
-#-- merge in relevant columns from participant.manifest
-dfs.export[["genomes"]] <- merge(dfs.export[["genomes"]],
-				 participant.manifest[,c("TrialNo", "PatNo", "PatientID"),],
-				 by = "PatientID",
-				 all.x = T)
-
-#-- write out to rd file
-saveRDS(dfs.export, "arctic-research-data.rds")
-
-#-- make list of dimenstions to check that have written correctly
-dims.export.ls <- lapply(dfs.export, function(x) c(nrow(x), ncol(x)))
-dims.export.df <- as.data.frame(do.call(rbind, dims.export.ls))
-names(dims.export.df) <- c("nrows", "ncols")
-
-#-- write out each of dfs in dfs.export
-lapply(seq_along(dfs.export), function(i) writefile(dfs.export[[i]], paste0("../research-datasets/arctic/", names(dfs.export)[i])))
-
-#-- CHECKS
-#-- get list of exported files
-exportedfiles <- list.files("../research-datasets/arctic", full.names = T)
-
-#-- get line and column counts of each exported file
-exported.dims.ls <- lapply(exportedfiles, function(x) c(linecount(x) - 1, colcount(x)))
-exported.dims.df <- as.data.frame(do.call(rbind, exported.dims.ls)) 
-row.names(exported.dims.df) <- gsub(".csv", "", basename(exportedfiles))
-
-#-- are there any differences
-dims.export.df[order(rownames(dims.export.df)),] == exported.dims.df[order(rownames(exported.dims.df)),]
